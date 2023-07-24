e7c00ad8cda414549ddcb23ae4ccfdf7fddc10da
prosazhin <prosazhin@gmail.com>
2023-07-24 23:43:34 +0200

    feat: husky install

---
+++ src/constants/main.ts
@@ -0,0 +1,8 @@
+const COLORS = ['primary', 'secondary', 'success', 'danger'];
+
+const COLOR_PRIMARY = 'primary';
+const COLOR_SECONDARY = 'secondary';
+const COLOR_SUCCESS = 'success';
+const COLOR_DANGER = 'danger';
+
+export default { COLORS, COLOR_PRIMARY, COLOR_SECONDARY, COLOR_SUCCESS, COLOR_DANGER };
